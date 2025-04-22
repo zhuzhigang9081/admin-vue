9159044a37f4761feb33d2e56aa529e87258f087
zhuzhigang <13383538433@163.com>
2025-04-22 16:15:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import login from './views/login/index.vue'
+</script>
+
+<template>
+  <div>
+    <div class="zhangsan text-3xl font-bold underline  w-20 h-20 bg-red-600 !text-gray-500">nihao </div>
+
+  </div>
+</template>
+
+<style scoped lang="less"></style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+        <div>
+            <h1>登录</h1>
+            <el-button type="primary">Primary</el-button>
+        </div>
+</template>
+    
+<script setup lang='ts'>
+    
+</script>
+    
+<style>
+    
+</style>
